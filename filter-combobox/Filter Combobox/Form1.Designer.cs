0504f0db080832c803c44fa4eaea6b1f2f3b9a5a
ekosuprastio <=>
2019-06-17 14:45:22 +0700

    Filter Combobox Datasource C# first commit

---
+++ filter-combobox/Filter Combobox/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Filter_Combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telephone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Telephone";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 82);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(155, 54);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Address";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ALL"});
+            this.comboBox1.Location = new System.Drawing.Point(96, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Name";
+            // 
+            // address
+            // 
+            this.address.HeaderText = "Address";
+            this.address.Name = "address";
+            this.address.Width = 150;
+            // 
+            // telephone
+            // 
+            this.telephone.HeaderText = "Telephone";
+            this.telephone.Name = "telephone";
+            // 
+            // lastname
+            // 
+            this.lastname.HeaderText = "Lastname";
+            this.lastname.Name = "lastname";
+            // 
+            // firstname
+            // 
+            this.firstname.HeaderText = "Firstname";
+            this.firstname.Name = "firstname";
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.firstname,
+            this.lastname,
+            this.telephone,
+            this.address});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 172);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(595, 236);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 420);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telephone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ filter-combobox/Filter Combobox/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections;
+
+namespace Filter_Combobox
+{
+    public partial class Form1 : Form
+    {
+        ConnectionDB con = new ConnectionDB();
+
+        //arraylist to getter and setter data
+        private static ArrayList ListID = new ArrayList();
+        private static ArrayList ListFirstname = new ArrayList();
+        private static ArrayList ListLastname = new ArrayList();
+        private static ArrayList ListTelephone = new ArrayList();
+        private static ArrayList ListAddress = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getDataComboBox()
+        {
+            con.Open();
+            string query = "select firstname from user";
+            MySqlDataReader row;
+            row = con.ExecuteReader(query);
+            if (row.HasRows)
+            {
+                while (row.Read())
+                {
+                    comboBox1.Items.Add(row["firstname"].ToString());
+                }
+
+            }
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getDataComboBox();
+            GetData();
+            if (ListID.Count > 0)
+            {
+                updateDatagrid();
+            }
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                con.Open();
+                string query = "select id,firstname,lastname,telephone,address from user";
+
+                //MySqlDataReader row;
+                MySqlDataReader row;
+                row = con.ExecuteReader(query);
+                if (row.HasRows)
+                {
+                    while (row.Read())
+                    {
+                        ListID.Add(row["id"].ToString());
+                        ListFirstname.Add(row["firstname"].ToString());
+                        ListLastname.Add(row["lastname"].ToString());
+                        ListTelephone.Add(row["telephone"].ToString());
+                        ListAddress.Add(row["address"].ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data not found");
+                }
+
+                con.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+
+        private void updateDatagrid()
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < ListID.Count; i++)
+            {
+                DataGridViewRow newRow = new DataGridViewRow();
+
+                newRow.CreateCells(dataGridView1);
+                newRow.Cells[0].Value = ListID[i];
+                newRow.Cells[1].Value = ListFirstname[i];
+                newRow.Cells[2].Value = ListLastname[i];
+                newRow.Cells[3].Value = ListTelephone[i];
+                newRow.Cells[4].Value = ListAddress[i];
+                dataGridView1.Rows.Add(newRow);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem.ToString() == "ALL")
+                {
+                    clearData();
+                    GetData();
+                    if (ListID.Count > 0)
+                    {
+                        updateDatagrid();
+                    }
+                }
+                else
+                {
+                    clearData();
+                    con.Open();
+                    string query = "select id,firstname,lastname,telephone,address from user where firstname = '" + comboBox1.SelectedItem.ToString() + "'";
+
+                    //MySqlDataReader row;
+                    MySqlDataReader row;
+                    row = con.ExecuteReader(query);
+                    if (row.HasRows)
+                    {
+                        while (row.Read())
+                        {
+                            ListID.Add(row["id"].ToString());
+                            ListFirstname.Add(row["firstname"].ToString());
+                            ListLastname.Add(row["lastname"].ToString());
+                            ListTelephone.Add(row["telephone"].ToString());
+                            ListAddress.Add(row["address"].ToString());
+                        }
+
+                        textBox2.Text = row["telephone"].ToString();
+                        textBox3.Text = row["address"].ToString();
+                        updateDatagrid();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not found");
+                    }
+
+                    con.Close();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        private void clearData()
+        {
+            ListID.Clear();
+            ListFirstname.Clear();
+            ListLastname.Clear();
+            ListAddress.Clear();
+            ListTelephone.Clear();
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
