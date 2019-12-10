a274ad88e1e9f68104b10ff28f54aa396c4388b2
ekosuprastio <=>
2019-12-10 14:08:08 +0700

    delegates first commit

---
+++ delegates/delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread summary = new Thread(o => { example.ThreadCalculate(); }); summary.Start();
+        }
+    }
+}
---
+++ delegates/delegates/example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class example
+    {
+        internal delegate void ThreadSummary(int val1, int val2);
+
+        public static void ThreadCalculate()
+        {
+            while (true)
+            {
+                Console.WriteLine("==========Summary===========");
+                string val1;
+                string val2;
+                Console.Write("Enter first integer: ");
+                val1 = Console.ReadLine();
+                Console.Write("Enter second integer: ");
+                val2 = Console.ReadLine();
+
+                ThreadSummary sum = new ThreadSummary(ProcessSummary);
+                sum.Invoke(Convert.ToInt32(val1), Convert.ToInt32(val2));
+            }
+        }
+
+        internal static void ProcessSummary(int val1, int val2)
+        {
+            int result = val1 + val2;
+            Console.WriteLine("Your Summary Input: {0}", result);
+            Console.WriteLine("============================");
+        }
+    }
+}
